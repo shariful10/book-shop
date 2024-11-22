d1d9a3f8ae14242a41e46c54295b6669c5fea06f
Md. Shariful Islam <freelancershariful10@gmail.com>
2024-11-23 01:24:37 +0600

    Refector

---
+++ dist/app/modules/book/book.route.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BookRoutes = void 0;
+const express_1 = __importDefault(require("express"));
+const book_controller_1 = require("./book.controller");
+const router = express_1.default.Router();
+router.post("/products", book_controller_1.BookController.createBook);
+exports.BookRoutes = router;
