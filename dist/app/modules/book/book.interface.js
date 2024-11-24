d33fb571192fd34d82efb9dcd168e94ab105a205
Md. Shariful Islam <freelancershariful10@gmail.com>
2024-11-24 16:25:05 +0600

    Refector

--- dist/app/modules/book/book.interface.js
+++ dist/app/modules/book/book.interface.js
@@ -1,6 +1,2 @@
 "use strict";
-// import { Model } from "mongoose";
 Object.defineProperty(exports, "__esModule", { value: true });
-// export interface BookModel extends Model<TBook> {
-//   isBookExists(id: string): Promise<TBook | null>;
-// }
